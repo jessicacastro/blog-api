e5100efc19e1d2d474bd19f6fd679083ae1d79cc
Jessica Castro <jessica.castro_storm@prestador.globo>
2022-08-04 11:41:43 -0300

    Inserindo alterações para o blog api funcionar corretamente

--- src/db/factories/postFactory.js
+++ src/db/factories/postFactory.js
@@ -1,9 +1,15 @@
 const { faker } = require('@faker-js/faker');
 
-module.exports = () => {
-    return {
-        title: faker.hacker.phrase(),
-        description: faker.lorem.paragraphs(),
-        createdAt: faker.date.past()
+module.exports = (quantity) => {
+    const posts = [];
+    for (let i = 0; i < quantity; i++) {
+        const post =  {
+            title: faker.hacker.phrase(),
+            description: faker.lorem.paragraphs(),
+            createdAt: faker.date.past()
+        }
+        posts.push(post);
     }
+
+    return posts;
 }
--- src/db/seeders/20220217012502-posts.js
+++ src/db/seeders/20220217012502-posts.js
@@ -5,11 +5,10 @@ const postFactory = require('../factories/postFactory');
 module.exports = {
   async up (queryInterface, Sequelize) {
     const posts = postFactory(10);
-    await queryInterface.bulkInsert('posts', posts, {});
-     
+    return await queryInterface.bulkInsert('posts', posts, {});
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('posts', null, {});
+    return await queryInterface.bulkDelete('posts', null, {});
   }
 };
